0dc65b53da7f25d1c334d763ad9e1aa597f6aea9
weionly <2959697175@qq.com>
2018-08-02 10:13:11 +0800

    魏德旺

--- src/main/java/com/hzit/shop/dao/Weidewang.java
+++ src/main/java/com/hzit/shop/dao/Weidewang.java
@@ -4,4 +4,7 @@ package com.hzit.shop.dao;
  * Created by 29596 on 2018/8/2.
  */
 public class Weidewang {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
