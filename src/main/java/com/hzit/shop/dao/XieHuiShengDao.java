cc98e366cbc83fdd14e1d3a8b333474477898cc2
qingtianzaishang <741322378@qq.com>
2018-08-02 12:59:45 +0800

    谢惠生的测帅程序

---
+++ src/main/java/com/hzit/shop/dao/XieHuiShengDao.java
@@ -0,0 +1,10 @@
+package com.hzit.shop.dao;
+
+/**
+ * Created by 74132 on 2018/8/2.
+ */
+public class XieHuiShengDao {
+    public void showShuai(){
+        System.out.println("你真的很帅！");
+    }
+}
