21169ce0e7f9bc4395106d44129c9720a6f5f0dd
littlexiaotimpro <a99996655@163.com>
2018-08-02 10:18:51 +0800

    littlexiaotimpro

--- src/main/java/com/hzit/shop/dao/HongChenDao.java
+++ src/main/java/com/hzit/shop/dao/HongChenDao.java
@@ -1,10 +1,7 @@
 package com.hzit.shop.dao;
 
-/**
- * @ClassName HongChenDao
- * @Description TODO
- * @Author XiaoSi
- * @Date 2018/8/210:03
- */
 public class HongChenDao {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
